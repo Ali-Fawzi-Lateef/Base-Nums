585a666f819abdde09f9a33ac52502fa90382ae6
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-08-31 14:55:32 +0300

    Update Main.cpp

--- Base Nums/Main.cpp
+++ Base Nums/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-string to_base(int number, int base) { // function for convertion
+string to_base(int number, int base) { // function for convertion.
 	string bases = "0123456789ABCDEF";// for converting the decimal to any base
 	string result = "";
 	while (number > 0) {
